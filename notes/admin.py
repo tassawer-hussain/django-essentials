3620d7a153563a5983a3a4dd53c52d17345c6367
Tassawer Hussain <t.tassawer@gmail.com>
2025-05-30 21:40:50 +0500

    Register the notes into the admin side

--- notes/admin.py
+++ notes/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
+from . import models
+
 # Register your models here.
+class NotesAdmin(admin.ModelAdmin):
+    list_display = ('title', 'text', 'created')
+
+admin.site.register(models.Notes, NotesAdmin)
