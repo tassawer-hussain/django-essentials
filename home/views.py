29a4255b8404a50550f74785131fc4c730bcf12d
Tassawer Hussain <t.tassawer@gmail.com>
2025-05-30 18:37:24 +0500

    Create a login restricted page

--- home/views.py
+++ home/views.py
@@ -2,8 +2,13 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 from datetime import datetime
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 def home(request):
     # return HttpResponse('Hello, world!')
     return render(request,  'home/welcome.html', { 'today': datetime.today()})
+
+@login_required
+def authorized(request):
+    return render(request, 'home/authorized.html', {})
