71862c9e5b4f9ac182876029b422c7e3a4652eb1
Tassawer Hussain <t.tassawer@gmail.com>
2025-05-30 18:13:44 +0500

    Pass data to the template file in the render function

--- home/views.py
+++ home/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from datetime import datetime
+
 # Create your views here.
 def home(request):
     # return HttpResponse('Hello, world!')
-    return render(request,  'home/welcome.html', {})
+    return render(request,  'home/welcome.html', { 'today': datetime.today()})
