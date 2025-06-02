d74a10782b2384ae2a5ccff1dbdd728b4765e4f3
Tassawer Hussain <t.tassawer@gmail.com>
2025-06-02 16:32:19 +0500

    Change the home view to class base view

--- home/urls.py
+++ home/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('home', views.home),
+    # path('home', views.home),
+    path('home', views.HomeView.as_view()),
     path('authorized', views.authorized),
 ]
--- home/views.py
+++ home/views.py
@@ -4,10 +4,16 @@ from django.http import HttpResponse
 from datetime import datetime
 from django.contrib.auth.decorators import login_required
 
+from django.views.generic import TemplateView
+
 # Create your views here.
-def home(request):
-    # return HttpResponse('Hello, world!')
-    return render(request,  'home/welcome.html', { 'today': datetime.today()})
+# def home(request):
+#     # return HttpResponse('Hello, world!')
+#     return render(request,  'home/welcome.html', { 'today': datetime.today()})
+
+class HomeView(TemplateView):
+    template_name = 'home/welcome.html'
+    extra_context = { 'today': datetime.today() }
 
 @login_required(login_url='admin/')
 def authorized(request):
