880f930652a427fd8999d48d189f7e761d6b2669
Tassawer Hussain <t.tassawer@gmail.com>
2025-06-02 17:04:46 +0500

    Use the ListView and DetailView for notes listing

--- notes/views.py
+++ notes/views.py
@@ -1,17 +1,28 @@
 from django.shortcuts import render
 from django.http import Http404
 
+
+from django.views.generic import DetailView, ListView
+
 from .models import Notes
 
 # Create your views here.
-def list(request):
-    all_notes = Notes.objects.all()
-    return render( request, 'notes/notes_list.html', { 'notes': all_notes } )
+# def list(request):
+#     all_notes = Notes.objects.all()
+#     return render( request, 'notes/notes_list.html', { 'notes': all_notes } )
 
+# def detail( request, pk ):
+#     try:
+#         note = Notes.objects.get( pk=pk )
+#     except Notes.DoesNotExist:
+#         raise Http404("Note doesn't exist")
+#     return render( request, 'notes/notes_detail.html', { 'note': note } )
 
-def detail( request, pk ):
-    try:
-        note = Notes.objects.get( pk=pk )
-    except Notes.DoesNotExist:
-        raise Http404("Note doesn't exist")
-    return render( request, 'notes/notes_detail.html', { 'note': note } )
+class NotesListView(ListView):
+    model = Notes
+    context_object_name = "notes" # defualt is object. This will be use in the template file to render the data
+    template_name = "notes/notes_list.html" # we can skip this because it's django naming standards
+
+class NotesDetailView(DetailView):
+    model = Notes
+    context_object_name = "note"
