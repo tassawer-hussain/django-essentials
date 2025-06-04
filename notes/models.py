7a55bb38556cfc22f495b2932f96947d53fa7b18
Tassawer Hussain <t.tassawer@gmail.com>
2025-06-04 18:30:27 +0500

    Update the notes model to add the user as a foreign key, make migration and test the changes using the shell commands

--- notes/models.py
+++ notes/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Notes(models.Model):
@@ -6,3 +7,4 @@ class Notes(models.Model):
     text = models.TextField()
     created = models.DateTimeField(auto_now_add=True)
     likes = models.PositiveSmallIntegerField(default=0)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="notes")
