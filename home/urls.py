252f7d5cbffff11002430f8068e596c0ce65ce57
Tassawer Hussain <t.tassawer@gmail.com>
2025-06-04 19:40:51 +0500

    Make the home the default url by passing the empty parameters to HomeView

--- home/urls.py
+++ home/urls.py
@@ -5,7 +5,7 @@ from . import views
 urlpatterns = [
     # path('home', views.home),
     # path('authorized', views.authorized),
-    path('home', views.HomeView.as_view(), name='home'),
+    path('', views.HomeView.as_view(), name='home'),
     path('authorized', views.AuthorizedView.as_view(), name='authorized'),
     path('login', views.LoginInterfaceView.as_view(), name='login'),
     path('logout', views.LogoutInterfaceView.as_view(), name='logout'),
