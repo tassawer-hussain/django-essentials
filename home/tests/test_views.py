1a262c9ebf72069650d8ebbf2a00c427e8951998
Tassawer Hussain <t.tassawer@gmail.com>
2025-06-13 17:25:35 +0500

    Test the Get endpoint of home

--- home/tests/test_views.py
+++ home/tests/test_views.py
@@ -1,4 +1,5 @@
 def test_home_view(client):
+    # Test the Get endpoint of home
     response = client.get(path='/')
     assert response.status_code == 200
     assert 'Welcome to SmartNotes!' in str(response.content)
